48731211106413607a067aa26f80d8b308e58eda
Luke Shumaker <lukeshu@sbcglobal.net>
2016-05-31 00:02:08 -0400

    fix
     - 'fix' the values of the _files variables before adjusting them
     - in the clean variables, std.*_files are public
     - support suffixing std.clean_files with / to rm -r it.
     - using double-colon rules breaks automatic dependencies from core
     - subdirs belongs to core, not this module
     - sort the dependencies of 'installdirs', for brevity

--- build-aux/Makefile.each.tail/10-std.mk
+++ build-aux/Makefile.each.tail/10-std.mk
@@ -15,7 +15,10 @@
 # Add some more defaults to the *_files variables
 std.clean_files += $(std.gen_files) $(std.cfg_files) $(std.out_files)
 
-# Make each of the standard variables relative to the correct directory 
+# Fix each variable at its current value to avoid any weirdness
+$(foreach c,src gen cfg out sys clean slow,$(eval std.$c_files := $$(std.$c_files)))
+
+# Make each of the standard variables relative to the correct directory
 std.src_files   := $(addprefix $(srcdir)/,$(std.src_files))
 std.gen_files   := $(addprefix $(srcdir)/,$(std.gen_files))
 std.cfg_files   := $(addprefix $(outdir)/,$(std.cfg_files))
@@ -23,19 +26,21 @@ std.out_files   := $(addprefix $(outdir)/,$(std.out_files))
 std.sys_files   := $(addprefix $(DESTDIR),$(std.sys_files))
 std.clean_files := $(addprefix $(outdir)/,$(std.clean_files))
 std.slow_files  := $(addprefix $(outdir)/,$(std.slow_files))
-std.subdirs     := $(addprefix $(outdir)/,$(std.subdirs))
 
 # Creative targets
-$(outdir)/build      :       $(std.out_files)
-$(outdir)/install    :       $(std.sys_files)
-$(outdir)/installdirs: $(dir $(std.sys_files))
+$(outdir)/build      :        $(std.out_files)
+$(outdir)/install    :        $(std.sys_files)
+$(outdir)/installdirs: $(sort $(dir $(std.sys_files)))
 
 # Destructive targets
-_std.uninstall/$(outdir)        := $(_std.sys_files)
-_std.mostlyclean/$(outdir)      := $(filter-out $(_std.slow_files) $(_std.cfg_files) $(_std.gen_files) $(_std.src_files),$(_std.clean_files))
-_std.clean/$(outdir)            := $(filter-out                    $(_std.cfg_files) $(_std.gen_files) $(_std.src_files),$(_std.clean_files))
-_std.distclean/$(outdir)        := $(filter-out                                      $(_std.gen_files) $(_std.src_files),$(_std.clean_files))
-_std.maintainer-clean/$(outdir) := $(filter-out                                                        $(_std.src_files),$(_std.clean_files))
-$(addprefix $(outdir)/,uninstall mostlyclean clean distclean maintainer-clean)::
-	$(RM) -- $(sort $(_std.$(@F)/$(@D)))
+_std.uninstall/$(outdir)        := $(std.sys_files)
+_std.mostlyclean/$(outdir)      := $(filter-out $(std.slow_files) $(std.cfg_files) $(std.gen_files) $(std.src_files),$(std.clean_files))
+_std.clean/$(outdir)            := $(filter-out                   $(std.cfg_files) $(std.gen_files) $(std.src_files),$(std.clean_files))
+_std.distclean/$(outdir)        := $(filter-out                                    $(std.gen_files) $(std.src_files),$(std.clean_files))
+_std.maintainer-clean/$(outdir) := $(filter-out                                                     $(std.src_files),$(std.clean_files))
+$(addprefix $(outdir)/,uninstall mostlyclean clean distclean maintainer-clean): %: %-hook
+	$(RM)    -- $(sort $(filter-out %/,$(_std.$(@F)/$(@D))))
+	$(RM) -r -- $(sort $(filter     %/,$(_std.$(@F)/$(@D))))
 	$(RMDIR_P) $(sort $(dir $(_std.$(@F)/$(@D)))) 2>/dev/null || $(TRUE)
+$(foreach t,uninstall mostlyclean clean distclean maintainer-clean, $(outdir)/$t-hook)::
+.PHONY: $(foreach t,uninstall mostlyclean clean distclean maintainer-clean, $(outdir)/$t-hook)
