dd84a8ab8f67cbc94aa3a427693970dbf3797e23
Luke Shumaker <lukeshu@sbcglobal.net>
2016-06-01 16:09:26 -0400

    (systemd) tidy

--- build-aux/Makefile.each.tail/20-systemd.mk
+++ build-aux/Makefile.each.tail/20-systemd.mk
@@ -1,3 +1,15 @@
 -include $(outdir)/$(DEPDIR)/*.P*
+
 std.clean_files += *.o *.lo .deps/ .libs/
-include $(topsrcdir)/am-pat-rules.mk
+
+$(outdir)/%.o: $(srcdir)/%.c | $(outdir)/.deps
+	$(AM_V_CC)$(COMPILE)   -c -o $@ $<
+
+$(outdir)/%.lo: $(srcdir)/%.c | $(outdir)/.deps
+	$(AM_V_CC)$(LTCOMPILE) -c -o $@ $<
+
+$(outdir)/.deps:
+	$(AM_V_at)$(MKDIR_P) $@
+
+$(outdir)/%.la:
+	$(AM_V_CCLD)$(LINK) $(filter-out .var%,$^)
