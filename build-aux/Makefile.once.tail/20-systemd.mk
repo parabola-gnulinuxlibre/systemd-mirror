aab3aab0b571d8ebb3316808f7da91fda86fa180
Luke Shumaker <lukeshu@sbcglobal.net>
2016-06-07 00:02:49 -0400

    stuff

--- build-aux/Makefile.once.tail/20-systemd.mk
+++ build-aux/Makefile.once.tail/20-systemd.mk
@@ -27,15 +27,11 @@ $(topsrcdir)/configure: $(topsrcdir)/configure.ac
 config_files = config.mk automake.mk autoconf.mk gnustandards.mk po/Makefile.in
 config_headers = config.h
 config_commands = depfiles libtool po/stamp-it
-$(addprefix $(topoutdir)/,config.status $(config_files) $(config_headers)): $(topoutdir)/%: $(topoutdir)/%.stamp
-$(topoutdir)/config.status.stamp: $(topsrcdir)/configure
+$(topoutdir)/config.status: $(topsrcdir)/configure
 	cd $(topoutdir) && ./config.status --recheck
-	test -f $(topoutdir)/$*
-	touch $@
-$(foreach f,$(config_files),$(topoutdir)/$f.stamp): $(topoutdir)/%.stamp: $(topoutdir)/config.status $(topsrcdir)/%.in
+$(addprefix $(topoutdir)/,$(config_files)): $(topoutdir)/%: $(topoutdir)/config.status $(topsrcdir)/%.in
 	cd $(topoutdir) && ./config.status --file=$*
-	test -f $(topoutdir)/$*
-	touch $@
+$(addprefix $(topoutdir)/,$(config_headers)): $(topoutdir)/%: $(topoutdir)/%.stamp
 $(foreach f,$(config_headers),$(topoutdir)/$f.stamp): $(topoutdir)/%.stamp: $(topoutdir)/config.status $(topsrcdir)/%.in
 	cd $(topoutdir) && ./config.status --header=$*
 	test -f $(topoutdir)/$*
