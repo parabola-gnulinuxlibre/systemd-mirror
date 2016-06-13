0fba659a87ec00d2f8b81b8cb0ce6a267428a15d
Luke Shumaker <lukeshu@sbcglobal.net>
2016-06-13 13:01:17 -0400

    re-add rootprefix,

--- move.sh
+++ move.sh
@@ -265,9 +265,6 @@ fixup_makefile() {
 	sed -r \
 	    -e '/^[^#	]*:/ { s|^(\s*)\S+/|\1$(outdir)/| }' \
 	    -e 's|^if (.*)|ifneq ($(\1),)|' \
-	    -e 's|rootprefix|prefix|g' \
-	    -e 's|rootbin|bin|g' \
-	    -e 's|rootlib|lib|g' \
 	    -e 's|--version-script=.*/([^/]+)\.sym|--version-script=$(srcdir)/\1.sym|g'
 }
 
