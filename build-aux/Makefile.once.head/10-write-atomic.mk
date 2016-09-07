3643b504c1078b3f5daef50e6f6d98b669681a60
Luke Shumaker <lukeshu@sbcglobal.net>
2016-09-07 01:20:54 -0400

    build-aux: add var and write-atomic modules

---
+++ build-aux/Makefile.once.head/10-write-atomic.mk
@@ -0,0 +1,4 @@
+mod.write-atomic.description = `write-atomic` auxiliary build script
+mod.write-atomic.files += $(topsrcdir)/build-aux/write-atomic
+
+WRITE_ATOMIC ?= $(topsrcdir)/build-aux/write-atomic
