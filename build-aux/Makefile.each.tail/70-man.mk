b585358da58463ca0e6fa1961e85340f27583f63
Luke Shumaker <lukeshu@sbcglobal.net>
2016-09-13 02:32:22 -0400

    mv build-aux/Makefile.each.tail/70-{sd,man}.mk

--- build-aux/Makefile.each.tail/70-man.mk
+++ build-aux/Makefile.each.tail/70-man.mk
@@ -21,38 +21,39 @@
 #  You should have received a copy of the GNU Lesser General Public License
 #  along with systemd; If not, see <http://www.gnu.org/licenses/>.
 
-mod.sd.depends += files write-atomic
+mod.man.description = (systemd) manpages
+mod.man.depends += am files write-atomic
 
-_sd.man_xml = $(foreach _sd.tmp,$(filter %.xml,$(files.src.src)),$(if $(findstring /,$(_sd.tmp)),,$(_sd.tmp)))
+_man.man_xml = $(foreach _man.tmp,$(filter %.xml,$(files.src.src)),$(if $(findstring /,$(_man.tmp)),,$(_man.tmp)))
 
-ifneq ($(_sd.man_xml),)
-#$(info $(outdir)/_sd.man_xml: «$(_sd.man_xml)»)
+ifneq ($(_man.man_xml),)
+#$(info $(outdir)/_man.man_xml: «$(_man.man_xml)»)
 
-$(srcdir)/Makefile-man.mk: $(topsrcdir)/tools/make-man-rules.py $(topsrcdir)/tools/xml_helper.py $(topsrcdir)/man/custom-entities.ent.in $(outdir)/.var._sd.man_xml $(call at.addprefix,$(srcdir),$(_sd.man_xml))
+$(srcdir)/Makefile-man.mk: $(topsrcdir)/tools/make-man-rules.py $(topsrcdir)/tools/xml_helper.py $(topsrcdir)/man/custom-entities.ent.in $(outdir)/.var._man.man_xml $(call at.addprefix,$(srcdir),$(_man.man_xml))
 	$(AM_V_GEN)$(PYTHON) $< $(filter %.xml,$^) | $(WRITE_ATOMIC) $@
 files.src.gen += Makefile-man.mk
 
-sd.MANPAGES =
-sd.MANPAGES_ALIAS =
+man.MANPAGES =
+man.MANPAGES_ALIAS =
 -include $(srcdir)/Makefile-man.mk
 
-_sd.XML_FILES = \
-	${patsubst %.1,%.xml,${patsubst %.3,%.xml,${patsubst %.5,%.xml,${patsubst %.7,%.xml,${patsubst %.8,%.xml,$(sd.MANPAGES)}}}}}
-sd.HTML_FILES = \
-	${_sd.XML_FILES:.xml=.html}
-sd.HTML_ALIAS = \
-	${patsubst %.1,%.html,${patsubst %.3,%.html,${patsubst %.5,%.html,${patsubst %.7,%.html,${patsubst %.8,%.html,$(sd.MANPAGES_ALIAS)}}}}}
+_man.XML_FILES = \
+	${patsubst %.1,%.xml,${patsubst %.3,%.xml,${patsubst %.5,%.xml,${patsubst %.7,%.xml,${patsubst %.8,%.xml,$(man.MANPAGES)}}}}}
+man.HTML_FILES = \
+	${_man.XML_FILES:.xml=.html}
+man.HTML_ALIAS = \
+	${patsubst %.1,%.html,${patsubst %.3,%.html,${patsubst %.5,%.html,${patsubst %.7,%.html,${patsubst %.8,%.html,$(man.MANPAGES_ALIAS)}}}}}
 
 ifneq ($(ENABLE_MANPAGES),)
 man_MANS = \
-	$(sd.MANPAGES) \
-	$(sd.MANPAGES_ALIAS)
+	$(man.MANPAGES) \
+	$(man.MANPAGES_ALIAS)
 
 noinst_DATA += \
-	$(sd.HTML_FILES) \
-	$(sd.HTML_ALIAS)
+	$(man.HTML_FILES) \
+	$(man.HTML_ALIAS)
 endif # ENABLE_MANPAGES
 
 at.subdirs += $(abspath $(topoutdir)/man)
 
-endif # _sd.man_xml
+endif # _man.man_xml
