33ab91cdc94926977f921c3a741df80d66ba6de9
Luke Shumaker <lukeshu@sbcglobal.net>
2017-01-14 23:40:09 -0500

    gnuconf: Was missing CPPFLAGS

--- build-aux/Makefile.once.head/10-gnuconf.mk
+++ build-aux/Makefile.once.head/10-gnuconf.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2016  Luke Shumaker
+# Copyright (C) 2016-2017  Luke Shumaker
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
@@ -100,6 +100,8 @@ TEXI2DVI ?= texi2dvi
 TEXI2DVIFLAGS ?=
 YACC ?= yacc
 YFLAGS ?= # YFLAGS instead of YACCFLAGS
+
+CPPFLAGS ?=
 
 LN_S ?= ln -s # TODO[2]
 
