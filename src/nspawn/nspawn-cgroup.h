6f8efd2d5daea788a2f4445cc208fa20b296e07b
Luke Shumaker <lukeshu@lukeshu.com>
2017-06-17 17:36:48 -0400

    nspawn: Go ahead and always decide the cgroup mounts in the outer child, not inner
    It mounts them in the outer child if !use_cgns, or inner child if use_cgns.
    Previously, it decided the mounts at the same place that it actually mounted
    them.  But, for simplicity and flexibility, always decide them in the outer
    child.

--- src/nspawn/nspawn-cgroup.h
+++ src/nspawn/nspawn-cgroup.h
@@ -31,8 +31,8 @@ typedef struct CGMounts {
 } CGMounts;
 
 int cgroup_setup(pid_t pid, CGroupUnified outer_cgver, CGroupUnified inner_cgver, uid_t uid_shift, bool keep_unit);
+int cgroup_decide_mounts(CGMounts *ret_mounts, CGroupUnified outer_cgver, CGroupUnified inner_cgver, bool use_cgns);
 int cgroup_mount_mounts(CGMounts mounts, bool use_cgns, uid_t uid_shift, const char *selinux_apifs_context);
 void cgroup_free_mounts(CGMounts *mounts);
 
-int mount_cgroups(const char *dest, CGroupUnified outer_cgver, CGroupUnified inner_cgver, bool userns, uid_t uid_shift, uid_t uid_range, const char *selinux_apifs_context, bool use_cgns);
 int mount_systemd_cgroup_writable(const char *dest, CGroupUnified inner_cgver);
